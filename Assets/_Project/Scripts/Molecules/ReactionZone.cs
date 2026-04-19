47b66d4b20a17d1d2302f4c789e22636ccd60df3
Anujith <128761904+Anujith1234@users.noreply.github.com>
2026-04-19 12:54:38 +0530

    Add bonding chamber and reaction zone molecule validation

---
+++ Assets/_Project/Scripts/Molecules/ReactionZone.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using UnityEngine;
+using VRMolecularLab.Data;
+using VRMolecularLab.XR;
+
+namespace VRMolecularLab.Molecules
+{
+    public class ReactionZone : MonoBehaviour
+    {
+        [Header("Data")]
+        [SerializeField] private MoleculeDatabase moleculeDatabase;
+
+        [Header("Debug")]
+        [SerializeField] private bool logMatches = true;
+
+        private readonly HashSet<AtomController> atomsInZone = new();
+
+        public IReadOnlyCollection<AtomController> AtomsInZone => atomsInZone;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent(out AtomController atomController))
+            {
+                return;
+            }
+
+            atomsInZone.Add(atomController);
+            EvaluateCurrentContents();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.TryGetComponent(out AtomController atomController))
+            {
+                return;
+            }
+
+            atomsInZone.Remove(atomController);
+            EvaluateCurrentContents();
+        }
+
+        public MoleculeDefinition EvaluateCurrentContents()
+        {
+            if (moleculeDatabase == null)
+            {
+                Debug.LogWarning("ReactionZone has no MoleculeDatabase assigned.", this);
+                return null;
+            }
+
+            Dictionary<AtomType, int> atomCounts = BuildAtomCounts();
+
+            MoleculeDefinition match = moleculeDatabase.FindMatchingMolecule(atomCounts);
+
+            if (logMatches)
+            {
+                if (match != null)
+                {
+                    Debug.Log($"Valid molecule detected: {match.MoleculeName} ({match.Formula})", this);
+                }
+                else if (atomCounts.Count > 0)
+                {
+                    Debug.Log("Current atom set does not match a valid molecule.", this);
+                }
+            }
+
+            return match;
+        }
+
+        private Dictionary<AtomType, int> BuildAtomCounts()
+        {
+            Dictionary<AtomType, int> atomCounts = new();
+
+            foreach (AtomController atom in atomsInZone)
+            {
+                if (atom == null)
+                {
+                    continue;
+                }
+
+                if (!atomCounts.ContainsKey(atom.AtomType))
+                {
+                    atomCounts[atom.AtomType] = 0;
+                }
+
+                atomCounts[atom.AtomType]++;
+            }
+
+            return atomCounts;
+        }
+    }
+}
